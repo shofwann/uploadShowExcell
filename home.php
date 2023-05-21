400bd0d2c65ad84e35febe3c0d12e3ac2b06072a
dispa intraday <dispatcherp3b@gmail.com>
2023-05-21 07:59:10 +0700

    ditambahkan kolom modifikasi dan button

--- home.php
+++ home.php
@@ -26,15 +26,27 @@
     </form>
 
     <script>
+        
+    function handleFile(e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
 
-function handleFile(e) {
-    var file = e.target.files[0];
-    var reader = new FileReader();
+        reader.onload = function (e) {
+            var data = new Uint8Array(e.target.result);
+            var workbook = XLSX.read(data, { type: 'array' });
 
-    reader.onload = function (e) {
-        var data = new Uint8Array(e.target.result);
-        var workbook = XLSX.read(data, { type: 'array' });
-        var sheet = workbook.Sheets[workbook.SheetNames[0]];
+            var html1 = generateTable(workbook, 0, 'lokasiManuverBebas', 'installManuverBebas');
+            document.getElementById('file-data').innerHTML = html1;
+
+            var html2 = generateTable(workbook, 1, 'lokasiManuverNormal', 'installManuverNormal');
+            document.getElementById('file-data2').innerHTML = html2;
+        };
+
+        reader.readAsArrayBuffer(file);
+    }
+
+    function generateTable(workbook, sheetIndex, lokasiName, installName) {
+        var sheet = workbook.Sheets[workbook.SheetNames[sheetIndex]];
         var jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
         var html = '<table>';
@@ -44,6 +56,7 @@ function handleFile(e) {
         html += '<th rowspan="2">Lokasi</th>';
         html += '<th colspan="3">Jam manuver Buka</th>';
         html += '<th rowspan="2">Instalasi</th>';
+        html += '<th rowspan="2">Modifikasi</th>';
         html += '</tr>';
         html += '<tr>';
         html += '<th>Remote</th>';
@@ -52,39 +65,41 @@ function handleFile(e) {
         html += '</tr>';
         html += '</thead>';
         html += '<tbody>';
-		
+
         for (var i = 0; i < jsonData.length; i++) {
-			html += '<tr>';
+            html += '<tr>';
             for (var j = 0; j < jsonData[i].length; j++) {
-				var cellValue = jsonData[i][j] || ''; // Jika nilai sel undefined, ganti dengan string kosong
-                if (j === 1) { // Kolom "Lokasi"
-                    html += '<td><input type="text" name="lokasiManuverBebas[]" value="' + cellValue + '"></td>';
+                var cellValue = jsonData[i][j] || ''; // Jika nilai sel undefined, ganti dengan string kosong
+                if (j === 0) { // Kolom "No."
+                    html += '<td>' + cellValue + '</td>';
+                } else if (j === 1) { // Kolom "Lokasi"
+                    html += '<td><input type="text" name="' + lokasiName + '[]" value="' + cellValue + '"></td>';
                 } else if (j === 5) { // Kolom "Instalasi"
-                    html += '<td><input type="text" name="installManuverBebas[]" value="' + cellValue + '"></td>';
+                    html += '<td><input type="text" name="' + installName + '[]" value="' + cellValue + '"></td>';
                 } else if (j === 2 || j === 3 || j === 4) {
-					if (cellValue !== '') {
-						html += '<td><input type="text" value="' + cellValue + '"></td>';
+                    if (cellValue !== '') {
+                        html += '<td><input type="text" value="' + cellValue + '"></td>';
                     } else {
-						html += '<td></td>'; // Kosongkan jika nilainya kosong
+                        html += '<td></td>'; // Kosongkan jika nilainya kosong
                     }
-                } else {
-					html += '<td>' + cellValue + '</td>';
                 }
             }
+            html += '<td>'; // Tambahkan kolom "Modifikasi" dengan nilai "coba"
+            html += '<button type="button" class="btn red" onclick="kurangBaris(this)">Remove</button>';
+            html += '<button type="button" class="btn green" onclick="sisip(this,'+lokasiName+'[],'+installName+'[])">Insert</button>';
+            html += '</td>'
             html += '</tr>';
         }
-		
+
         html += '</tbody>';
         html += '</table>';
-		
-        document.getElementById('file-data').innerHTML = html;
 
-		var sheet2 = workbook.Sheets[workbook.SheetNames[1]];
-		console.log(sheet2)
-    };
+        return html;
+    }
 
-    reader.readAsArrayBuffer(file);
-}
+
+
+
 
 
 
